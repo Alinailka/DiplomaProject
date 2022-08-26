53c11601da8ceae432fcb3eb55817e09efbdad99
Alinailka <alina-il@mail.ru>
2022-08-26 16:47:56 +0700

    first test is green

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,9 +1,15 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.Duration;
 
@@ -14,6 +20,16 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestClass {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {
         Configuration.holdBrowserOpen = true;
@@ -23,27 +39,28 @@ public class TestClass {
     @Test
     @DisplayName("Купить с одобренной картой")
     void buyApprovedCard() {
+
         $x("//*[text()=\"Купить\"]").click();
-        $$(byText("Номер карты")).filter(visible).first().setValue("4444 4444 4444 4441");//DataGenerator.getApprovedCard());
-        $(byText("Месяц")).setValue(DataGenerator.getMonth());
-        $(byText("Год")).setValue(DataGenerator.getYear());
-        $(byText("Владелец")).setValue(DataGenerator.getRandomName());
-        $(byText("CVC/CVV")).setValue(DataGenerator.getCVC());
+        $("[placeholder=\"0000 0000 0000 0000\"]").setValue("4444 4444 4444 4441");
+        $("[placeholder=\"08\"]").setValue(DataGenerator.getMonth());
+        $("[placeholder=\"22\"]").setValue(DataGenerator.getYear());
+        $x(("//span[contains(.,'Владелец')]/following-sibling::span/input")).setValue(DataGenerator.getRandomName());
+        $("[placeholder=\"999\"]").setValue("111");
         $(byText("Продолжить")).click();
-        $(byText("Операция одобрена Банком")).should(Condition.visible,Duration.ofSeconds(15));
+        $(withText("Операция одобрена Банком")).should(Condition.visible,Duration.ofSeconds(15));
     }
 
     @Test
     @DisplayName("Купить с отклоненной картой")
     void buyDeclinedCard() {
-        $(byText("Купить")).click();
-        $(byText("Номер карты")).setValue(DataGenerator.getDeclinedCard());
-        $(byText("Месяц")).setValue(DataGenerator.getMonth());
-        $(byText("Год")).setValue(DataGenerator.getYear());
-        $(byText("Владелец")).setValue(DataGenerator.getRandomName());
-        $(byText("CVC/CVV")).setValue(DataGenerator.getCVC());
+        $x("//*[text()=\"Купить\"]").click();
+        $("[placeholder=\"0000 0000 0000 0000\"]").setValue("4444 4444 4444 4442");
+        $("[placeholder=\"08\"]").setValue(DataGenerator.getMonth());
+        $("[placeholder=\"22\"]").setValue(DataGenerator.getYear());
+        $x(("//span[contains(.,'Владелец')]/following-sibling::span/input")).setValue(DataGenerator.getRandomName());
+        $("[placeholder=\"999\"]").setValue("111");
         $(byText("Продолжить")).click();
-        $(byText("Ошибка! Банк отказал в проведении операции")).should(Condition.visible);
+        $(withText("Ошибка! Банк отказал в проведении операции")).should(Condition.visible,Duration.ofSeconds(15));
     }
 
     @Test
